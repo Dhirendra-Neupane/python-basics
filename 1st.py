e05121cd8c1d0b2da2c8ac36cc95c002d88463e0
Dhirendra-Neupane <dhirendraneupane083@gmail.com>
2026-05-09 17:01:08 -0500

    added other operators, still needs while loop

--- 1st.py
+++ 1st.py
@@ -1,13 +1,37 @@
 print("Everything starts from 0")
 print("Let's make a basic calculator")
+
 x = int(input("Enter the first number: "))
 y = int(input("Enter the second number: "))
 
-print()
+def printing():
+    print("Choose one of the follwing operations(1-4): ")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Divison")
+    print("-1. Exit")
+    user_choice = int(input())
+    return user_choice
 
-print("Choose one of the follwing operations(1-4): ")
-print("1. Addition")
-user_choice = int(input())
-if user_choice == 1:
+
+print()
+user_choice1 = printing()
+if user_choice1 == 1:
     z = x + y
     print(f"The sum of the numbers is: {z}")
+elif user_choice1 == 2:
+    z = x - y
+    print(f"{x} - {y} is equal to {z}")
+elif user_choice1 == 3:
+    z = x * y
+    print(f"The product of {x} and {y} is {z}")
+elif user_choice1 == 4:
+    z = x / y
+    print(f"{x} divided by {y} results in {z}")
+elif user_choice1 == -1:
+    print("Exitting calculator")
+else:
+    print("Wrong input!!! try again!!")
+
+print("Thank you for using our calculator.")
