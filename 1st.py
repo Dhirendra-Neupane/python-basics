ae0cf353daad0c66cb6d817dcb57fab6c2c486c7
Dhirendra-Neupane <dhirendraneupane083@gmail.com>
2026-05-09 15:23:36 -0500

    done some changes

--- 1st.py
+++ 1st.py
@@ -1,1 +1,8 @@
-print("Everythins starts from 0")
+print("Everything starts from 0")
+print("Let's make a basic calculator")
+x = input("Enter the first number")
+y = input("Enter the second number")
+
+z = x +y
+
+print(f"The sum of the numbers is: {z}")
