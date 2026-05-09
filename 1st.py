a024d2012d6a23c9b4419565d6ad420c362cb94c
Dhirendra-Neupane <dhirendraneupane083@gmail.com>
2026-05-09 15:32:24 -0500

    Added an if choice for adding

--- 1st.py
+++ 1st.py
@@ -1,8 +1,15 @@
 print("Everything starts from 0")
 print("Let's make a basic calculator")
-x = input("Enter the first number")
-y = input("Enter the second number")
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
 
-z = x +y
+print()
 
-print(f"The sum of the numbers is: {z}")
+print("Choose one of the follwing operations(1-4): ")
+print("1. Addition")
+user_choice = input()
+
+
+if user_choice == 1:
+    z = x + y
+    print(f"The sum of the numbers is: {z}")
