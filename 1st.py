d16edc2d7216880c56af2def9fcfc237067ae3c4
Dhirendra-Neupane <dhirendraneupane083@gmail.com>
2026-05-15 23:54:22 -0500

    FINAL version of my basic calculator

--- 1st.py
+++ 1st.py
@@ -1,8 +1,7 @@
 print("Everything starts from 0")
 print("Let's make a basic calculator")
 
-x = int(input("Enter the first number: "))
-y = int(input("Enter the second number: "))
+
 
 def printing():
     print("Choose one of the follwing operations(1-4): ")
@@ -15,25 +14,29 @@ def printing():
 while True:
     printing()
     user_choice1 = int(input("Enter a choice from above options: "))
-    if user_choice1 == 1:
-        z = x + y
-        print(f"The sum of the numbers is: {z}")
-    elif user_choice1 == 2:
-        z = x - y
-        print(f"{x} - {y} is equal to {z}")
-    elif user_choice1 == 3:
-        z = x * y
-        print(f"The product of {x} and {y} is {z}")
-    elif user_choice1 == 4:
-        if y == 0:
-            print("Can't divide by zero")
-        else:
-            z = x / y
-            print(f"{x} divided by {y} results in {z}")
+    if user_choice1 != -1:
+        x = int(input("Enter the first number: "))
+        y = int(input("Enter the second number: "))
+        
+        if user_choice1 == 1:
+            z = x + y
+            print(f"The sum of the numbers is: {z}")
+        elif user_choice1 == 2:
+            z = x - y
+            print(f"{x} - {y} is equal to {z}")
+        elif user_choice1 == 3:
+            z = x * y
+            print(f"The product of {x} and {y} is {z}")
+        elif user_choice1 == 4:
+            if y == 0:
+                print("Can't divide by zero")
+            else:
+                z = x / y
+                print(f"{x} divided by {y} results in {z}")
     elif user_choice1 == -1:
         print("Exitting calculator")
         break
     else:
         print("Wrong input!!! try again!!")
-
+    
 print("Thank you for using our calculator.")
