d589edf3e0d50764b3845c354bfeb80cb901d05f
Dhirendra-Neupane <dhirendraneupane083@gmail.com>
2026-05-16 00:31:29 -0500

    Less gogit add . we are finally into some good coding project

---
+++ calculator-app/main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("Calculator-1st Project")
+
+window.geometry("450x600")
+
+window.mainloop()
+
+def click(value):
+    print(value)
+
+click(7)
