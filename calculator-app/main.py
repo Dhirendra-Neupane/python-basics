a9f38dcb8eb554d0ab1da00be5fa28dfae0e40b4
Dhirendra-Neupane <dhirendraneupane083@gmail.com>
2026-05-16 01:41:00 -0500

    Added a buton but haven't made it function yet

--- calculator-app/main.py
+++ calculator-app/main.py
@@ -6,9 +6,18 @@ window.title("Calculator-1st Project")
 
 window.geometry("450x600")
 
-window.mainloop()
-
 def click(value):
     print(value)
 
-click(7)
+button1 = tk.Button(window, text = "1", width = 4, height = 4,
+                     font = ("Arial", 20),  bg = "black", fg = "white")
+button1.grid(row = 0, column = 1)
+
+
+
+
+window.mainloop()
+
+
+
+click(7) 
